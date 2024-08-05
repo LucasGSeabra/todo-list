dc0aa6eb58abc4aee04177d64277978705ba0ff8
LucasGSeabra <lucasgseabra@gmail.com>
2024-08-05 10:15:47 -0300

    Update adds reducers to todoSlice

--- src/redux/slices/todoSlice/index.ts
+++ src/redux/slices/todoSlice/index.ts
@@ -15,7 +15,47 @@ const todoSlice = createSlice({
   name: "todosSlice",
   initialState: initialState,
   reducers: {
+    createTodo: (state, action: PayloadAction<string>) => {
+      const now = Date.now();
+      const newTodo: Todo = {
+        id: `${now}${action.payload}`,
+        task: action.payload,
+        completed: false,
+      };
+
+      state.todos.push(newTodo);
+    },
+    updateTodo: (state, action: PayloadAction<Todo>) => {
+      state.todos = state.todos.map((todo) =>
+        todo.id === action.payload.id ? action.payload : todo
+      );
+    },
+    deleteTodo: (state, action: PayloadAction<Todo>) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
+    },
+    markAllAsComplete: (state, action: PayloadAction<Todo[]>) => {
+      const todosToComplete = getTodosIds(action.payload);
+
+      state.todos = state.todos.map((todo) =>
+        todosToComplete.includes(todo.id) ? { ...todo, completed: true } : todo
+      );
+    },
+    deleteAllCompleted: (state, action: PayloadAction<Todo[]>) => {
+      const todosToDelete = getTodosIds(action.payload);
+
+      state.todos = state.todos.filter((todo) => !todosToDelete.includes(todo.id));
+    },
   },
 });
+
+export const {
+  createTodo,
+  deleteTodo,
+  updateTodo,
+  deleteAllCompleted,
+  markAllAsComplete,
+} = todoSlice.actions;
+
+export const getAllTodos = (state: RootState) => state.todos;
 
 export default todoSlice.reducer;
