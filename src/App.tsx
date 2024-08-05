10366390229a504ab5372cd2085630354218551c
LucasGSeabra <lucasgseabra@gmail.com>
2024-08-05 10:22:14 -0300

    Update add cssBaseline to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
 import Header from "./component/Header";
 
 function App() {
   return (
     <div>
+      <CssBaseline />
       <Header />
     </div>
   );
