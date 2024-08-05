1bac8510728a104a2d99cc513f43bd6833a6abdf
LucasGSeabra <lucasgseabra@gmail.com>
2024-08-05 10:43:49 -0300

    Update adds ignoreActions to store

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "../slices/todoSlice";
-import { persistReducer } from "redux-persist";
+import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
@@ -12,6 +12,12 @@ const persistedReducer = persistReducer(persistConfig, todosReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
