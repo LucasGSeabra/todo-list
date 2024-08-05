f5ded46e7e2af5c6a337d89af5a9fbc39184201c
LucasGSeabra <lucasgseabra@gmail.com>
2024-08-05 10:16:32 -0300

    Update add redux providers to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import App from "./App";
+import persistStore from "redux-persist/lib/persistStore";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { App } from './App.jsx'
+let persistor = persistStore(store);
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<React.StrictMode><App /></React.StrictMode>)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
